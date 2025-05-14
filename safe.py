c9e644deee7680a29ebceb379d333b1e4f407b5b
Aiman Baharna <ab@clearmatics.com>
2025-05-15 00:56:31 +0400

    Checkpoint

--- safe.py
+++ safe.py
@@ -2,6 +2,7 @@
 
 import click
 
+from eth_typing import ChecksumAddress
 from safe_eth.eth import EthereumClient
 from safe_eth.safe import Safe
 # from safe_eth.safe import SafeTx
@@ -15,13 +16,38 @@ def get_safe():
     return Safe(safe_address, ethereum_client)  # pyright: ignore[reportAbstractUsage, reportArgumentType]
 
 
-@click.command()
+@click.group()
 def main():
     print("hello")
     return
     # safe = get_safe()
     # safe_info = safe.retrieve_all_info()
     # print(safe_info)
+
+
+@click.command()
+@click.option("--safe", "-s", required=True, help="address of the Safe")
+@click.option("--nonce", "-n", type=int, required=True, help="nonce of the Safe")
+@click.option("--to", "-t", help="destination address")
+@click.option(
+    "--abi",
+    "contract_abi_path",
+    type=click.Path(exists=True),
+    help="contract ABI file (for a contract tx)",
+)
+@click.option("--method", "-m", help="contract method to call (for a contract tx)")
+@click.option("--value", "-v", help="value in ETH", default="0")
+@click.argument("parameters", nargs=-1)
+def create(safe: str, none: int):
+    """
+    Create a SafeTx from the options and parameters passed in.
+
+    For a smart contract transaction, set the destination address to the contract address.
+    """
+    pass
+
+
+main.add_command(create)
 
 
 if __name__ == "__main__":
