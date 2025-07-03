eae85048e48fe87f5c3636872b274fe3ac2a6dcf
Aiman Baharna <ab@clearmatics.com>
2025-07-03 19:26:22 +0400

    Improve devenv test launcher
    Support multiple Python versions via uv. This requires bypassing devenv,
    which seems to have `UV_PYTHON_DOWNLOADS=never` set, which would block
    downloading any other Python version.

--- devenv.nix
+++ devenv.nix
@@ -20,7 +20,21 @@
     uv.sync.enable = true;
   };
 
-  scripts.run-tests.exec = "uv run pytest -l -s -v --no-header --disable-warnings ./test";
+  env.PYTHON_VERSIONS = "3.11 3.12 3.13";
+  env.PYTEST_COMMAND = "pytest -l -s -v --no-header --disable-warnings ./test";
+  scripts.testrun.exec = "uv run $PYTEST_COMMAND";
+  scripts.testrun-multi.exec = ''
+    UV_PYTHON_DOWNLOADS=automatic  # disabled by devenv/Nix
+    for PYTHON_VERSION in $PYTHON_VERSIONS; do
+      uv run --python $PYTHON_VERSION $PYTEST_COMMAND
+    done
+  '';
+
+  scripts.pyinstall.exec = ''
+    set -ux
+    uv python install $PYTHON_VERSIONS
+  '';
+
 
   # See full reference at https://devenv.sh/reference/options/
 }
