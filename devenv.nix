fe18cda58cc7d1e12d0a4759604902ea8b14a2b5
Aiman Baharna <ab@clearmatics.com>
2025-07-03 19:29:26 +0400

    Add new check, format, and lint scripts

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,10 @@
 }:
 
 {
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.taplo
+  ];
 
   env.UV_PYTHON = "${config.env.DEVENV_PROFILE}/bin/python";
 
@@ -19,6 +22,30 @@
     uv.enable = true;
     uv.sync.enable = true;
   };
+
+  env.SOURCE_DIRS = "src/simple_safe test";
+
+  scripts.check.exec = ''
+    set -ux
+    uv sync --dev
+    uv run ruff check $SOURCE_DIRS
+    uv run pyright $SOURCE_DIRS
+  '';
+
+  scripts.format.exec = ''
+    set -ux
+    uv sync --dev
+    uv run ruff check --fix --select I $SOURCE_DIRS
+    uv run ruff format $SOURCE_DIRS
+    RUST_LOG=warn taplo fmt pyproject.toml
+  '';
+
+  scripts.lint.exec = ''
+    set -ux
+    uv run ruff check --diff --select I $SOURCE_DIRS
+    uv run ruff format --check --diff $SOURCE_DIRS
+    RUST_LOG=warn taplo fmt --check --diff pyproject.toml
+  '';
 
   env.PYTHON_VERSIONS = "3.11 3.12 3.13";
   env.PYTEST_COMMAND = "pytest -l -s -v --no-header --disable-warnings ./test";
