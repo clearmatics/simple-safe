ceedcb916120f8d3b13ee74bdf9432b09576c656
Aiman Baharna <ab@clearmatics.com>
2025-08-03 15:05:28 +0300

    Reduce unnecessary output in devenv scripts

--- devenv.nix
+++ devenv.nix
@@ -29,31 +29,28 @@
   env.SOURCE_DIRS = "src/simple_safe test";
 
   scripts.autofix.exec = ''
-    set -ux
     uv sync -q --dev
-    uv run ruff check --fix
-    uv run ruff check --fix --select I $SOURCE_DIRS
+    uv run -q ruff check --fix
+    uv run -q ruff check --fix --select I $SOURCE_DIRS
   '';
 
   scripts.check.exec = ''
-    set -ux
     uv sync -q --dev
-    uv run ruff check $SOURCE_DIRS
-    uv run pyright $SOURCE_DIRS
+    uv run -q ruff check $SOURCE_DIRS
+    uv run -q pyright $SOURCE_DIRS
   '';
 
   scripts.format.exec = ''
     set -ux
     uv sync -q --dev
-    uv run ruff check --fix --select I $SOURCE_DIRS
-    uv run ruff format $SOURCE_DIRS
+    uv run -q ruff check --fix --select I $SOURCE_DIRS
+    uv run -q ruff format $SOURCE_DIRS
     RUST_LOG=warn taplo fmt pyproject.toml
   '';
 
   scripts.lint.exec = ''
-    set -ux
-    uv run ruff check --diff --select I $SOURCE_DIRS
-    uv run ruff format --check --diff $SOURCE_DIRS
+    uv run -q ruff check --diff --select I $SOURCE_DIRS
+    uv run -q ruff format --check --diff $SOURCE_DIRS
     RUST_LOG=warn taplo fmt --check --diff pyproject.toml
   '';
 
@@ -61,21 +58,20 @@
     set -ux
     IMPORT_LOG=$(mktemp)
     uv run -q python -X importtime -m simple_safe.safe 2>$IMPORT_LOG
-    uv run tuna $IMPORT_LOG
+    uv run -q tuna $IMPORT_LOG
   '';
 
   env.PYTHON_VERSIONS = "3.11 3.12 3.13";
   env.PYTEST_COMMAND = "pytest -l -s -v --no-header --disable-warnings ./test";
-  scripts.runtests.exec = "uv run $PYTEST_COMMAND";
+  scripts.runtests.exec = "uv run -q $PYTEST_COMMAND";
   scripts.runtests-multi.exec = ''
     UV_PYTHON_DOWNLOADS=automatic  # disabled by devenv/Nix
     for PYTHON_VERSION in $PYTHON_VERSIONS; do
-      uv run --python $PYTHON_VERSION $PYTEST_COMMAND
+      uv run -q --python $PYTHON_VERSION $PYTEST_COMMAND
     done
   '';
 
   scripts.pyinstall.exec = ''
-    set -ux
     uv python install $PYTHON_VERSIONS
   '';
 
